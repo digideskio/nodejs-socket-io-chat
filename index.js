4230e49f1c1f4ea3d5e8d95ecabc1bb02218a80c
Chathu Vishwajith <4evercreations@gmail.com>
2015-09-29 19:58:24 +0530

    Completed basic chat

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
-
 
 http.listen(port,function(){
       console.log('%s: Node server started on %d...', Date(Date.now()),port);
